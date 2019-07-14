0328069382e5ba39816dae9d2584f60381450a47
Kowies <kowieski.m98@gmail.com>
2019-07-14 20:40:47 +0200

    Rename file from lib/brute_tsp/py to lib/brute_tsp_solver.py

--- lib/brute_tsp_solver.py
+++ lib/brute_tsp_solver.py
@@ -4,7 +4,7 @@ from lib.tsp_state import TSPState
 from lib.tsp_solver import TSPSolver
 
 
-class BruteTSP(TSPSolver):
+class BruteTSPSolver(TSPSolver):
     def __init__(self, tsp):
         self.initial_state = TSPState.initial_state(1, tsp=tsp)
 
@@ -21,15 +21,9 @@ class BruteTSP(TSPSolver):
                 if best_trip == None or current.length < best_trip.length:
                     best_trip = current
 
-            # only for analysis
-            if visited_count % 10000 == 0:
-                print(len(current.reconstruct_path()), current.length, visited_count)
-
             queue.extend(current.extend())
 
         if best_trip:
             return best_trip.reconstruct_path(), best_trip.length, visited_count
-
-
 
         return [], float("inf"), float("inf")
